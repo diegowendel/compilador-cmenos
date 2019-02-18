22fbf2929960cd738dc22f808464f9f282bffcc0
diegoferreira <dgnnsx@gmail.com>
2019-02-17 22:35:34 -0300

    Compilação local funcionando

--- compiler.js
+++ compiler.js
@@ -1,34 +1,47 @@
 const editorIn = ace.edit("editor-in");
 const editorOut = ace.edit("editor-out");
 
-$(function() {
-    editorIn.setTheme("ace/theme/xcode");
-    editorIn.session.setMode("ace/mode/c_cpp");
-    editorIn.setOptions({
-        enableBasicAutocompletion: true, // the editor completes the statement when you hit Ctrl + Space
-        enableLiveAutocompletion: true, // the editor completes the statement while you are typing
-        showPrintMargin: true, // hides the vertical limiting strip
-        fontSize: "100%" // ensures that the editor fits in the environment
-    });
+const compile = (evt) => {
+  evt.preventDefault();
+  evt.stopPropagation();
+  const codeInC = editorIn.getValue();
 
-    editorOut.setTheme("ace/theme/monokai");
-    editorOut.session.setMode("ace/mode/c_cpp");
-    editorOut.setReadOnly(true);
-    editorOut.renderer.$cursorLayer.element.style.display = "none"
-    editorOut.setOptions({
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: true,
-        fontSize: "100%",
-        highlightActiveLine: false,
-        highlightGutterLine: false,
-        readOnly: true,
-        showPrintMargin: false
-    });
+  $.ajax({
+    url: 'http://localhost:3000/compile',
+    type: 'POST',
+    data: {code: codeInC},
+    success: (response) => {
+      editorOut.setValue(response.code, 1);
+    },
+    error: () => {
+      console.log('Request failed');
+    },
+  });
+};
 
-    editorOut.setValue(str, 1);
+$(() => {
+  editorIn.setTheme("ace/theme/xcode");
+  editorIn.session.setMode("ace/mode/c_cpp");
+  editorIn.setOptions({
+    enableBasicAutocompletion: true, // the editor completes the statement when you hit Ctrl + Space
+    enableLiveAutocompletion: true, // the editor completes the statement while you are typing
+    showPrintMargin: true, // hides the vertical limiting strip
+    fontSize: "100%" // ensures that the editor fits in the environment
+  });
+
+  editorOut.setTheme("ace/theme/monokai");
+  editorOut.session.setMode("ace/mode/c_cpp");
+  editorOut.setReadOnly(true);
+  editorOut.renderer.$cursorLayer.element.style.display = "none"
+  editorOut.setOptions({
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true,
+    fontSize: "100%",
+    highlightActiveLine: false,
+    highlightGutterLine: false,
+    readOnly: true,
+    showPrintMargin: false
+  });
 });
 
-$("#btn-compile").click(function() {
-    const strIn = editorIn.getValue();
-    editorOut.setValue(strIn, 1);
-});
+$("#btn-compile").click(compile);
