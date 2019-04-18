8274d77515665bc4d8bd8b216338a449ad25eb1d
diegoferreira <dgnnsx@gmail.com>
2019-04-17 23:40:54 -0300

    Ajuste na porta do servidor

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT_HTTP: 8080,
-  PORT_HTTPS: 3000
+  PORT_HTTPS: 6000 // 6000: Escolhido de forma a não conflitar com outros possíveis processos Node executados no servidor
 };
